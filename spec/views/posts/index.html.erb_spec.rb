046a309343570070c7243ac1037a8c502fb3b76a
Brenda Wihogora <brewihogora23@gmail.com>
2022-10-11 23:47:57 +0200

    Add intergration some more test of post index

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -19,3 +19,26 @@ RSpec.feature 'User Posts Index Page', type: :feature do
   it "I can see the user's username" do
     expect(page).to have_content(@user.name)
   end
+ it "I can see the number of posts the user has written" do
+    expect(page).to have_content('Number of Posts: 1')
+  end
+  it "I can see a post's title" do
+    expect(page).to have_content(@first_post.title)
+  end
+  it "I can see some of the post's body" do
+    expect(page).to have_content(@first_post.text)
+  end
+  it "I can see the first comments on a post" do
+    expect(page).to have_content('Hi John!')
+  end
+  it "I can see how many comments a post has" do
+    expect(page).to have_content("Comments: #{@first_post.comments_counter}")
+  end
+  it "I can see how many likes a post has" do
+    expect(page).to have_content("Likes: #{@first_post.likes_counter}")
+  end
+  it "When I click on a post, it redirects me to that post's show page" do
+    click_link(@first_post.title)
+    expect(page).to have_current_path user_post_path(@user.id, @first_post.id)
+  end
+end
