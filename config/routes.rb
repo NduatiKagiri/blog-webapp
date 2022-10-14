d9d22d923ac62d1aaa9ecc3e9847750094f68daf
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-10-15 00:00:46 +0300

    Make requested changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: ''
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
+      delete 'logout', to: 'sessions#destroy'
     end
   end
 
